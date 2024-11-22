b7f176d86d02ea18ab553ad689dc5bf7654f25cd
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-11-22 15:21:00 -0300

    Efeitos

---
+++ js/scrollrevel.js
@@ -0,0 +1,36 @@
+ScrollReveal({ reset: true });
+
+/*Sessao de textos*/
+    ScrollReveal().reveal('#Texto', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'left',
+        }
+    );
+/*Sessao de textos*/    
+/*Sessao cards */
+    ScrollReveal().reveal('.Card01', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'top',
+        }
+    );
+    ScrollReveal().reveal('#cards02', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+        }
+    );
+    ScrollReveal().reveal('#cards04', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'bottom',
+        }
+    );
+    ScrollReveal().reveal('.card02', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'left',
+        }
+    );
+    ScrollReveal().reveal('#cards05', 
+        {    
+            duration: 1000, reset: true, distance: '100px' , origin: 'right',
+        }
+    );
+/*Sessao cards */
